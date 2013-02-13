0d81bf5b54fe607debd82067d9f598cfcd304168
Mason Wendell <mason@canarypromo.com>
2013-02-13 09:50:32 -0500

    full-width style guide; touch menu button

--- build/public/js/kit/kit-source.jquery.js
+++ build/public/js/kit/kit-source.jquery.js
@@ -120,7 +120,7 @@ function foldSection($el, $dir) {
 */
 (function($){
   if($('body.documentation').length == 0){
-    $('header.kit-ui').prepend('<a class="menu-trigger"></a>');
+    $('header.kit-ui').prepend('<a class="menu-trigger">m</a>');
     var jPM = $.jPanelMenu();
     jPM.on();
   }
